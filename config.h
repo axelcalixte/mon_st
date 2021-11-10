4fd64b4a7f75bcb982c5a655d91349fa8680ff36
axelcalixte <axelcalixte@disroot.org>
2021-11-10 23:31:17 +0100

    rose-pine

--- config.h
+++ config.h
@@ -113,31 +113,50 @@ float alpha = 0.95;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
-    "#3b4252", /* black   */
-    "#bf616a", /* red     */
-    "#a3be8c", /* green   */
-    "#ebcb8b", /* yellow  */
-    "#81a1c1", /* blue    */
-    "#b48ead", /* magenta */
-    "#88c0d0", /* cyan    */
-    "#e5e9f0", /* white   */
+    /* "#3b4252",  black   */
+    /* "#bf616a",  red     */
+    /* "#a3be8c",  green   */
+    /* "#ebcb8b",  yellow  */
+    /* "#81a1c1",  blue    */
+    /* "#b48ead",  magenta */
+    /* "#88c0d0",  cyan    */
+    /* "#e5e9f0",  white   */
+    "#403c58",
+    "#ea6f91",
+    "#9bced7",
+    "#f1ca93",
+    "#018993",
+    "#c3a5e6",
+    "#eabbb9",
+    "#faebd7",
 
     /* 8 bright colors */
-    "#4c566a", /* black   */
-    "#bf616a", /* red     */
-    "#a3be8c", /* green   */
-    "#ebcb8b", /* yellow  */
-    "#81a1c1", /* blue    */
-    "#b48ead", /* magenta */
-    "#8fbcbb", /* cyan    */
-    "#eceff4", /* white   */
+    /* "#4c566a",  black   */
+    /* "#bf616a",  red     */
+    /* "#a3be8c",  green   */
+    /* "#ebcb8b",  yellow  */
+    /* "#81a1c1",  blue    */
+    /* "#b48ead",  magenta */
+    /* "#8fbcbb",  cyan    */
+    /* "#eceff4",  white   */
+    "#403c58",
+    "#ea6f91",
+    "#9bced7",
+    "#f1ca93",
+    "#018993",
+    "#c3a5e6",
+    "#eabbb9",
+    "#faebd7",
 
     [255] = 0,
 
     /* more colors can be added after 255 to use with DefaultXX */
-    "#2e3440", /* background */
-    "#d8dee9", /* foreground */
-    "black",
+    /*"#2e3440",  background */
+    /*"#d8dee9",  foreground */
+    /*"black", */
+    "#1f1d29",
+    "#ffffff",
+    "#ffffff",
 };
 
 /*
@@ -147,8 +166,10 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 // unsigned int defaultbg = 258;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 256;
+// static unsigned int defaultcs = 257;
+// static unsigned int defaultrcs = 256;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 
 /*
  * Default shape of cursor
